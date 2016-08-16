c0968afcdaf7ebc42255542c499593618a7fcb5f
richbliu <richbliu@gmail.com>
2016-08-15 21:22:18 -0400

    Update mc.modules.animation.js

--- js/mc.modules.animation.js
+++ js/mc.modules.animation.js
@@ -1,5 +1,5 @@
  /* start GENERAL CUSTOMIZATION PROPERTIES */
-var templateBaseURL         = "";/*"http://themes.mediacreed.com/html/synergy/"*/ 
+var templateBaseURL         = "rbliu.com";/*"http://themes.mediacreed.com/html/synergy/"*/ 
 var themeColor              = "#098bc7";
 var menuActive              = true;
 var menuHoverActive         = true; /* it will change to false if menuActive == true. If 'menuActive' 
